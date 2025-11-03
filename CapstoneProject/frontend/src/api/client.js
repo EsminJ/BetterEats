c82605326baa27a6d6e95e9a0db8aead2be8be4c
Nicholas Cheung <nmcheung2004@gmail.com>
2025-11-03 01:14:26 -0500

    refactoring some more

--- CapstoneProject/frontend/src/api/client.js
+++ CapstoneProject/frontend/src/api/client.js
@@ -1,13 +1,7 @@
 import axios from 'axios';
-
-// IMPORTANT: If you are testing on a physical Android device,
-// you must replace 'localhost' with your computer's local IP address.
-// On Windows, run `ipconfig`. On Mac/Linux, run `ifconfig` or `ip addr`.
 const apiClient = axios.create({
-  // Use http, not https, for local development
-  //not my ip
+  // test server local IP
   baseURL: 'http://192.168.1.152:8000/api', 
-  // This helps manage cookies for sessions
   withCredentials: true,
 });
 
