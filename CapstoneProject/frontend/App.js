a538af4bedc2e45e308bf5447f85d08344aa0ee4
shresthapreen@gmail.com <Shresthapreen@gmail.com>
2025-12-01 20:04:19 -0500

    Fix merge conflicts and resolve duplicate screen error

--- CapstoneProject/frontend/App.js
+++ CapstoneProject/frontend/App.js
@@ -14,10 +14,6 @@ import { AuthContext, AuthProvider } from './src/context/AuthContext';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import AiCoachScreen from './src/screens/AiCoachScreen'; // new tab for AI Coach
 import ProfileScreen from './src/screens/ProfileScreen'; // added profile page
-
-import Ionicons from '@expo/vector-icons/Ionicons';
-import AiCoachScreen from './src/screens/AiCoachScreen'; // new tab for AI Coach
-import ProfileScreen from './src/screens/ProfileScreen'; // added profile page
 import GoalSetupScreen from './src/screens/GoalSetupScreen'; 
 
 const Stack = createNativeStackNavigator();
@@ -104,9 +100,6 @@ function RootNavigator() {
               options={{ headerTitle: 'Set Your Goals' }} 
             />
 
-            <Stack.Screen name="AddFood" component={AddFoodScreen} options={{ presentation: 'modal', headerTitle: 'Add Custom Food', headerTitleStyle: { fontWeight: '700', fontSize: 18, color: '#333'}, headerTitleAlign: 'center', }} />
-            <Stack.Screen name="Camera" component={CameraScreen} options={{ headerTitle: 'Scan Meal', headerTitleStyle: { fontWeight: '700', fontSize: 18, color: '#333'}, headerTitleAlign: 'center' }} />
-            <Stack.Screen name="Profile" component={ProfileScreen} options={{ headerTitle: 'My Profile', headerTitleStyle: { fontWeight: '700', fontSize: 18, color: '#333'}, headerTitleAlign: 'center' }} />
 
           </>
         ) : (
